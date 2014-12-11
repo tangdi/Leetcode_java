0a51115d7dfb3ef646c792d928aa301df00032f5
Di Tang <di.tang@sap.com>
2014-12-11 21:11:08 +0800

    finally solve it

--- src/RemoveDuplicatesfromSortedListII.java
+++ src/RemoveDuplicatesfromSortedListII.java
@@ -4,6 +4,60 @@ import IntersectionofTwoLinkedLists.ListNode;
 
 public class RemoveDuplicatesfromSortedListII {
 	public ListNode deleteDuplicates(ListNode head) {
+		if(head == null) return null;
+		ListNode newHead = null;
+		ListNode lastUnique = null;
+		ListNode pre = head;
+		ListNode node = head.next;
+		boolean repeat = false;
+		
+		while(node != null){
+			
+			if(pre.val == node.val){
+				repeat = true;
+				node = node.next;
+				continue;
+			}
+			
+			if(repeat && pre.val != node.val){
+				if(lastUnique != null){
+					lastUnique.next = node;
+				}
+				pre = node;
+				node = node.next;
+				
+				repeat = false;
+				continue;
+				
+			}
+			if(!repeat && pre.val != node.val){
+				
+				lastUnique = pre;
+				if(newHead == null){
+					newHead = lastUnique;
+				}
+				pre = node;
+				node = node.next;
+				continue;
+			}
+			
+			}
+		
+		if(lastUnique == null){
+			if(repeat){
+				return null;
+			}else{
+				return pre;
+			}
+		}
+		
+		if(repeat){
+			lastUnique.next = null;
+		}else{
+			lastUnique.next = pre;
+		}
+		
+		return newHead;
 		
 	}
 }
